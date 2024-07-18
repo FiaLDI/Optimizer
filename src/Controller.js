9075e22030404d96765ef33191acfe180dca82cf
FiaLDI <leon.youtube2017@yandex.ru>
2024-07-18 11:20:09 +0300

    0.5 core

--- src/Controller.js
+++ src/Controller.js
@@ -7,44 +7,44 @@ const process = require('process');
 const controller = new AbortController();
 const { signal } = controller;
 
-
 let targetDir = path.resolve(__dirname);
-let targetfile;
+const adressPage = 'localhost:3001/pages/';
+let curPage = '1'
+let curPages = []
 let RootPATH = __dirname;
 let curProcess;
 let isHTML = false;
+let configPath;
 
 exports.root = (req, res)=> {
-    let curdir = parseDir.parserDir(targetDir)
-    let curfile = parseDir.parserFile(targetDir)
+    
+    
     res.render('index', {
         title: 'app',
-        curDir: curdir,
-        curfile: curfile,
-        file: 'http://localhost:3001/',
-        filetext: targetfile,
-        isHTML: isHTML
+        curDir: parseDir.parserDir(targetDir),
+        curfile: parseDir.parserFile(targetDir),
+        file: 'http://' +adressPage + curPage,
+        isHTML: isHTML,
+        curPages: curPages,
+        isServerStart: !isHTML
+    }, (err, html) => {
+        if (err) return err
+        fs.readFile(path.resolve(RootPATH, 'config', 'paths.json'), (err, data)=> {
+            configPath = JSON.parse(data)[0];
+            curPages = configPath.pages
+        });
+        res.send(html)
     })
 }
 
 exports.rootDir = express.static('./')
 
-exports.font = (req, res) => {
-    res.sendFile(`${__dirname}/font/` + req.params["font"])
-}
+
 exports.css = (req, res)=> {
     res.sendFile(`${RootPATH}/css/style.css`)
 }
-exports.csss = (req, res)=> {
-    res.sendFile(`${__dirname}/css/` + req.params["csss"])
-}
-exports.img = (req, res)=> {
-    res.sendFile(`${__dirname}/img/` + req.params["imgg"])
-}
-exports.script = (req, res)=> {
-    res.sendFile(`${__dirname}/app/` + req.params["jsfiles"])
-}
 
+// file system
 exports.changePath = (req, res) => {
     if(!req.body) return res.sendStatus(400);
     let curCommand = JSON.parse(JSON.stringify(req.body));
@@ -70,43 +70,165 @@ exports.rootPath = (req, res) => {
     res.redirect('/')
 }
 
-exports.openFile = (req, res) => {
-    if(!req.body) return res.sendStatus(400);
-    let curCommand = JSON.parse(JSON.stringify(req.body));
-    let pathToFile = path.resolve(targetDir, curCommand.but1)
-    let pathToFileStart = path.resolve(targetDir, 'server.js')
-    isHTML = false
-    let format = curCommand.but1.split('.').at(-1),
-        command = "const express = require('express');const process = require('process');const app = express();app.use('/', express.static('./'));app.listen(3001, ()=>console.log(process.pid));"
+// exports.openFile = (req, res) => {
+//     if(!req.body) return res.sendStatus(400);
+//     let curCommand = JSON.parse(JSON.stringify(req.body));
+//     let pathToFile = path.resolve(targetDir, curCommand.but1)
+//     let pathToFileStart = path.resolve(targetDir, 'server.js')
+//     isHTML = false
+//     let format = curCommand.but1.split('.').at(-1),
+//         command = "const express = require('express');const process = require('process');const app = express();app.use('/', express.static('./'));app.listen(3001, ()=>console.log(process.pid));"
     
-    fs.readFile(pathToFile, (err, data)=> {
-        targetfile = data.toString()
-    })
+//     fs.readFile(pathToFile, (err, data)=> {
+//         targetfile = data.toString()
+//     })
 
-    if (curProcess != undefined) {
+//     if (curProcess != undefined) {
         
-        process.kill(curProcess)
-        curProcess = undefined
-    }
-    console.log(curProcess)
-    if (format == 'html') {
+//         process.kill(curProcess)
+//         curProcess = undefined
+//     }
+//     console.log(curProcess)
+//     if (format == 'html') {
         
-        isHTML = true
-        fs.writeFile(pathToFileStart, data=command, (err) => {
-            if(err) {
+//         isHTML = true
+//         fs.writeFile(pathToFileStart, data=command, (err) => {
+//             if(err) {
+//                 return;
+//             }
+//             child_process.exec(`node ${pathToFileStart}`, {
+//                 cwd: targetDir, 
+//                 signal: signal,
+//                 timeout: 1000
+//             }, (err, stdout, stderr) => {
+//                 curProcess = stdout;
+//             })
+
+            
+//         })
+//     }
+    
+//     res.redirect('/')
+// }
+
+exports.setPath = (req, res) => {
+    
+    let server = "const express = require('express');const process = require('process');const app = express();app.use('/', express.static('./'));app.listen(3001, ()=>console.log(process.pid));"
+    let pakage = {
+        "name": "1",
+        "version": "1.0.0",
+        "description": "",
+        "main": "App.js",
+        "scripts": {
+          "test": "echo \"Error: no test specified\" && exit 1",
+          "start": "node ./server.js"
+        },
+        "author": "",
+        "license": "ISC",
+        "dependencies": {
+          "express": "^4.19.2"
+        }
+      }
+    fs.readFile(path.resolve(RootPATH, 'config', 'paths.json'), (err, data)=> {
+        configPath = JSON.parse(data)[0];
+
+        
+        
+        configPath.targetServerPath = path.resolve(targetDir, 'opt')
+        configPath.pages = parseDir.parserDir(path.resolve(configPath.targetServerPath, 'pages'));
+
+        if (fs.existsSync(path.resolve(configPath.targetServerPath, 'server.js')) && 
+            fs.existsSync(path.resolve(configPath.targetServerPath, 'package.json'))) {
+                fs.writeFile(path.resolve(RootPATH, 'config', 'paths.json'), data=JSON.stringify([configPath]), (err)=> {
+                    if (err) return err
+                })    
+                return;
+        }
+
+        fs.mkdir(path.resolve(targetDir, 'opt'), () => {
+            fs.mkdir(path.resolve(targetDir, 'opt', 'pages'), () => {
+            })
+
+            fs.writeFile(path.resolve(configPath.targetServerPath, 'server.js'), data=server, (err) => {
+                if(err) {
+                    return;
+                }
+                fs.writeFile(path.resolve(configPath.targetServerPath, 'package.json'), data=JSON.stringify(pakage), (err) => {
+                    if(err) {
+                        return;
+                    }
+                    fs.writeFile(path.resolve(RootPATH, 'config', 'paths.json'), data=JSON.stringify([configPath]), (err)=> {
+                        if (err) return err
+                    })
+                })
+            })
+        })
+        
+    })
+    res.redirect('/')
+}
+
+exports.downloadDep = (req, res) => {
+
+    fs.exists(configPath.targetServerPath, (e)=> {
+        if (e) {
+            if (fs.existsSync(path.resolve(configPath.targetServerPath, 'node_modules'))) {
+                console.log('even')
                 return;
             }
-            child_process.exec(`node ${pathToFileStart}`, {
-                cwd: targetDir, 
+
+            if (fs.existsSync(path.resolve(configPath.targetServerPath, 'server.js')) && 
+                fs.existsSync(path.resolve(configPath.targetServerPath, 'package.json'))) {
+                    child_process.exec(`npm i`, {
+                        cwd: configPath.targetServerPath, 
+                        signal: signal,
+                        timeout: 1000
+                    }, (err, stdout, stderr) => {
+                        curProcess = stdout;
+                    })
+            }
+        }
+    })
+
+    
+
+        
+    res.redirect('/')
+}
+
+exports.startServer = (req, res) => {
+    console.log(configPath)
+    isHTML = true;
+    if (fs.existsSync(path.resolve(configPath.targetServerPath, 'node_modules')) &&
+        fs.existsSync(path.resolve(configPath.targetServerPath, 'server.js')) && 
+        fs.existsSync(path.resolve(configPath.targetServerPath, 'package.json'))) {
+            child_process.exec(`node server.js`, {
+                cwd: configPath.targetServerPath, 
                 signal: signal,
                 timeout: 1000
             }, (err, stdout, stderr) => {
                 curProcess = stdout;
+                console.log(curProcess)
             })
-
             
-        })
-    }
-    
+    } 
+    res.redirect('/')
+}
+
+exports.stopServer = (req, res) => {
+   
+    if (curProcess != undefined) {
+        process.kill(curProcess);
+        curProcess = undefined;
+        isHTML = false;
+    };
+    res.redirect('/')
+}
+
+exports.selectPage = (req, res) => {
+    if(!req.body) return res.sendStatus(400);
+    let curCommand = JSON.parse(JSON.stringify(req.body));
+    curPage = String(curCommand.but1)
+
     res.redirect('/')
 }
--- src/Router.js
+++ src/Router.js
@@ -6,20 +6,17 @@ const controller = require('./Controller')
 
 router.use('/', controller.rootDir)
 router.get('/', controller.root)
-
-// router.get('/font/:font', controller.font)
 router.get('/mycss', controller.css);
-// router.get('/css/:csss', controller.csss);
-// router.get('/img/:imgg', controller.img);
-// router.get('/ru/:imgg', controller.img);
-// router.get('/app/:jsfiles', controller.script);
 
 
-
-router.post("/changepath", urlencodedParser, controller.changePath)
+router.post("/changepath", urlencodedParser, controller.changePath);
 router.post("/rootpath", urlencodedParser, controller.rootPath);
 router.post("/back", urlencodedParser, controller.backPath);
-
-router.post("/openfile", urlencodedParser, controller.openFile);
+// router.post("/openfile", urlencodedParser, controller.openFile);
+router.post("/setpath",  urlencodedParser, controller.setPath);
+router.post("/downloaddep", urlencodedParser, controller.downloadDep);
+router.post("/startserver", urlencodedParser, controller.startServer);
+router.post("/stopserver", urlencodedParser, controller.stopServer);
+router.post("/selectpage", urlencodedParser, controller.selectPage);
 
 module.exports = router;
